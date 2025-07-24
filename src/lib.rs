c97644c4a532054f8ec2168289eb31fb633fd9a3
ktiays <cuansky@gmail.com>
2025-07-24 22:51:53 +0800

    Expose dependencies to top level

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
-pub mod additive_arithmetic;
-pub mod spring;
-pub mod vector_arithmetic;
+mod additive_arithmetic;
+mod spring;
+mod vector_arithmetic;
+
+pub use additive_arithmetic::AdditiveArithmetic;
+pub use spring::Spring;
+pub use vector_arithmetic::VectorArithmetic;
